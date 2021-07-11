fff70b3a91d6ffb78eb7227126445b8202a02767
goodhub1 <86038785+goodhub1@users.noreply.github.com>
2021-07-11 17:57:54 +0800

    Create HttpClient.java

---
+++ week03/src/HttpClient.java
@@ -0,0 +1,53 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClient {
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        //
+        HttpGet httpGet = new HttpGet("http://127.0.0.1:8888/");
+        httpGet.addHeader("mao", "soul");
+        //
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("status:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("content length:" + responseEntity.getContentLength());
+                System.out.println("content entity:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
