f647cd3ca2e207651c9589bb71e910e130110857
goodhub1 <86038785+goodhub1@users.noreply.github.com>
2021-07-31 11:03:09 +0800

    Create init.sql

---
+++ week06/init.sql
@@ -0,0 +1,23 @@
+create table user
+(
+id bigint AUTO_INCREMENT,
+user_name varchar(16),
+user_address varchar(16),
+PRIMARY KEY ( `id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table product
+(
+id bigint AUTO_INCREMENT,
+product_name varchar(16),
+product_price float,
+PRIMARY KEY ( `id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table order
+(
+id bigint AUTO_INCREMENT,
+user_id bigint ,
+product_id bigint,
+PRIMARY KEY ( `id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
